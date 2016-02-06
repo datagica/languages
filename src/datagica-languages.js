58b4c3885f2d4fba9f6d25260afe62b1f864d3cf
Julian Bilcke <julian.bilcke@gmail.com>
2016-02-06 15:40:32 +0100

    removed dependency to Babel

--- src/datagica-languages.js
+++
@@ -1,4 +0,0 @@
-// TODO put this in a separate project on github
-export const all_languages = require('../data/all-languages.json');
-
-export const active_languages = all_languages.filter(lang => (typeof lang.ietf === 'string'));
--- tests/all.js
+++ tests/all.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 chai.use(require('chai-fuzzy'));
 const expect = chai.expect;
 
-import { all_languages, active_languages } from '../../lib/datagica-languages';
+const datagicaLanguages = require("../lib/datagica-languages");
 
 // local functions
 function pretty(obj) {
@@ -13,8 +13,8 @@ function pretty(obj) {
 describe('@datagica/languages', () => {
 
   const languages_stats = {
-    all_languages: all_languages.length,
-    active_languages: active_languages.length,
+    all_languages: datagicaLanguages.all_languages.length,
+    active_languages: datagicaLanguages.active_languages.length,
     // popular_languages: popular_languages.length
   };
   console.log(pretty(languages_stats));
