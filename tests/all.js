ff0d507a724760bd67b4716703382fdbd5ffd221
Julian Bilcke <julian.bilcke@gmail.com>
2016-05-22 19:39:44 +0200

    cleaned the dataset

--- tests/all.js
+++ tests/all.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 chai.use(require('chai-fuzzy'));
 const expect = chai.expect;
 
-const datagicaLanguages = require("../lib/datagica-languages");
+const langs = require("../lib/datagica-languages");
 
 // local functions
 function pretty(obj) {
@@ -12,11 +12,15 @@ function pretty(obj) {
 
 describe('@datagica/languages', () => {
 
-  const languages_stats = {
-    all_languages: datagicaLanguages.all_languages.length,
-    active_languages: datagicaLanguages.active_languages.length,
-    // popular_languages: popular_languages.length
-  };
-  console.log(pretty(languages_stats));
+  it('should have the correct number of languages', () => {
+    const stats = {
+      all: langs.all.length,
+      withIETF: langs.withIETF.length,
+      ietf: Object.keys(langs.byIETF).length
+    };
+    console.log(pretty(stats));
+
+    expect(stats).to.be.like({ all: 550, withIETF: 58, ietf: 58 })
+  })
 
 });
